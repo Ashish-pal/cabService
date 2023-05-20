9e57d8d9da6c9f44b2bf6628e9a6493d6a858e13
Ashish Pal <ak987982@gmail.com>
2023-05-20 16:16:48 +0530

    Added Multiple Rides and Total Fare

--- src/main/java/cabService/CabServiceGen.java
+++ src/main/java/cabService/CabServiceGen.java
@@ -1,5 +1,7 @@
 package cabService;
 
+import java.util.List;
+
 public class CabServiceGen {
     private static final double COST_PER_KILOMETER = 10.0;
     private static final double COST_PER_MINUTE = 1.0;
@@ -8,5 +10,13 @@ public class CabServiceGen {
     public double calculateFare(double distance, double time) {
         double fare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
         return Math.max(fare, MINIMUM_FARE);
+    }
+
+    public double calculateTotalFare(List<Ride> rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
     }
 }
--- src/test/java/cabService/CabServiceTest.java
+++ src/test/java/cabService/CabServiceTest.java
@@ -1,12 +1,17 @@
 package cabService;
+
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CabServiceTest {
+    CabServiceGen invoiceGenerator = new CabServiceGen();
+
     @Test
     public void testCalculateFare() {
-        CabServiceGen invoiceGenerator = new CabServiceGen();
-
         double fare1 = invoiceGenerator.calculateFare(10.0, 30.0);
         assertEquals(130.0, fare1);
 
@@ -21,5 +26,24 @@ public class CabServiceTest {
 
         double fare5 = invoiceGenerator.calculateFare(15.0, 0.0);
         assertEquals(150.0, fare5);
+    }
+
+    @Test
+    public void CalculateTotalFareTest() {
+        List<Ride> rides1 = new ArrayList<>();
+        rides1.add(new Ride(10.0, 30.0));
+        rides1.add(new Ride(5.0, 15.0));
+        rides1.add(new Ride(2.0, 5.0));
+        double totalFare1 = invoiceGenerator.calculateTotalFare(rides1);
+        assertEquals(210.0, totalFare1);
+
+        List<Ride> rides2 = new ArrayList<>();
+        rides2.add(new Ride(15.0, 45.0));
+        double totalFare2 = invoiceGenerator.calculateTotalFare(rides2);
+        assertEquals(195.0, totalFare2);
+
+        List<Ride> rides3 = new ArrayList<>();
+        double totalFare3 = invoiceGenerator.calculateTotalFare(rides3);
+        assertEquals(0.0, totalFare3);
     }
 }
