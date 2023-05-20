25f733bc504472d816bc8bfbf44d4f29d0d1c821
Ashish Pal <ak987982@gmail.com>
2023-05-20 15:41:22 +0530

    Added Cab Service Welcome Message

---
+++ src/main/java/cabService/CabServiceGen.java
@@ -0,0 +1,7 @@
+package cabService;
+
+public class CabServiceGen {
+    public static void main(String[] args) {
+        System.out.println("The cab service is a subscription based cab service model.");
+    }
+}
